5ace3e058a7a90ac414963c75cca94b2ab8de26f
way <tangdibt@163.com>
2015-04-14 19:41:06 +0800

    init

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+base_dir=`pwd`
+tmp_dir=$base_dir/tmp
+
+rm -rf $tmp_dir 
+mkdir -p $tmp_dir
+git clone -b 0.9.2 https://github.com/apache/thrift.git $tmp_dir  
+if [ $? -ne 0 ]
+then
+    echo -e "\033[31m thrift: download from git error \033[0m"
+    exit 1
+fi
+
+cd $tmp_dir
+./bootstrap.sh \
+    && ./configure --enable-tests=no --enable-tutorial=no --with-php=no --with-python=no --with-lua=no --prefix=${base_dir} --enable-shared=no \
+        && make && make install
+if [ $? -ne 0 ]
+then
+    echo -e "\033[31m thrift: make error \033[0m"
+    exit 1
+fi
+rm -rf $tmp_dir
+
+
